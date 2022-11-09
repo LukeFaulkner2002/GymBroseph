d05f930a0afa373cc4cbd66b85bb84c3d81e43b1
Zackary Kiener <kienerz07@icloud.com>
2022-11-09 16:46:30 +0000

    minor api changes

--- backend/server.js
+++ backend/server.js
@@ -1,114 +1,113 @@
 //Required stuff
-const express = require('express');
-const path = require("path");
-const bodyParser = require('body-parser');
-const cors = require('cors');          
+import express from "express";
+import { resolve } from "path";
+import { json } from "body-parser";
+import cors from "cors";
+import blueimp from "blueimp-md5";
+
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.set('port', (process.env.PORT || 5000));
-
-var blueimp = require('blueimp-md5');
+app.set("port", process.env.PORT || 5000);
 
 //MongoDB stuff
-require('dotenv').config();
+require("dotenv").config();
 const url = process.env.MONGODB_URI;
-const MongoClient = require('mongodb').MongoClient;
+import { MongoClient } from "mongodb";
 const client = new MongoClient(url);
 client.connect();
- 
+
 //Set up production env
-if (process.env.NODE_ENV === 'production') 
-{
-  app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("frontend/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(resolve(__dirname, "frontend", "build", "index.html"));
+	});
 }
 
 //API and extra
 app.use(cors());
-app.use(bodyParser.json());
-app.use((req, res, next) => 
-{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
+app.use(json());
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+	next();
 });
 
-app.post('/api/register', async (req, res, next) =>
-{
-  const { firstName, lastName, login, password } = req.body;
-  const newUser = {FirstName:firstName,LastName:lastName, Login:login, Password:blueimp(password), UserId:5};
-  var error = '';
+app.post("/api/register", async (req, res, next) => {
+	const { firstName, lastName, login, password } = req.body;
+	const newUser = {
+		FirstName: firstName,
+		LastName: lastName,
+		Login: login,
+		Password: blueimp(password),
+		UserId: 5,
+	};
+	var error = "";
 
-  try
-  {
-    const db = client.db('Cards');
-    const result = db.collection('Users').insertOne(newUser);
-  }
-  catch(e)
-  {
-    error = e.toString();
-  }
+	try {
+		const db = client.db("Cards");
+		const result = db.collection("Users").insertOne(newUser);
+	} catch (e) {
+		error = e.toString();
+	}
 
-  var ret = { firstName:firstName, lastName:lastName, login:login, password:blueimp(password), error: error };
-  res.status(200).json(ret);
+	var ret = {
+		firstName: firstName,
+		lastName: lastName,
+		login: login,
+		password: blueimp(password),
+		error: error,
+	};
+	res.status(200).json(ret);
 });
 
-app.post('/api/login', async (req, res, next) => 
-{
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-  var error = '';
-  const { login, password } = req.body;
-  const db = client.db("Cards");
-  const results = await db.collection('Users').find({Login:login,Password:blueimp(password)}).toArray();
-  var id = -1;
-  var fn = '';
-  var ln = '';
-  if( results.length > 0 )
-  {
-    id = 1;
-    fn = results[0].FirstName;
-    ln = results[0].LastName;
-  }
-  var ret = { id:'1', firstName:fn, lastName:ln, error:''};
-  res.status(200).json(ret);
+app.post("/api/login", async (req, res, next) => {
+	// incoming: login, password
+	// outgoing: id, firstName, lastName, error
+	var error = "";
+	const { login, password } = req.body;
+	const db = client.db("Cards");
+	const results = await db
+		.collection("Users")
+		.find({ Login: login, Password: blueimp(password) })
+		.toArray();
+	var id = -1;
+	var fn = "";
+	var ln = "";
+	if (results.length > 0) {
+		id = 1;
+		fn = results[0].FirstName;
+		ln = results[0].LastName;
+	}
+	var ret = { id: "1", firstName: fn, lastName: ln, error: "" };
+	res.status(200).json(ret);
 });
 
-app.post('/api/searchcards', async (req, res, next) => 
-{
-  // incoming: userId, search
-  // outgoing: results[], error
-  var error = '';
-  const { userId, search } = req.body;
-  var _search = search.trim();
-  
-  const db = client.db("Cards");
-  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
- 
+// app.post('/api/searchcards', async (req, res, next) =>
+// {
+//   // incoming: userId, search
+//   // outgoing: results[], error
+//   var error = '';
+//   const { userId, search } = req.body;
+//   var _search = search.trim();
 
-  var _ret = [];
-  for( var i=0; i<results.length; i++ )
-  {
-    _ret.push( results[i].Card );
-  }
-  
-  var ret = {results:_ret, error:error};
-  res.status(200).json(ret);
+//   const db = client.db("Cards");
+//   const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
+
+//   var _ret = [];
+//   for( var i=0; i<results.length; i++ )
+//   {
+//     _ret.push( results[i].Card );
+//   }
+
+//   var ret = {results:_ret, error:error};
+//   res.status(200).json(ret);
+// });
+
+app.listen(PORT, () => {
+	console.log("Server listening on port " + PORT);
 });
-
-
-app.listen(PORT, () => 
-{
-  console.log('Server listening on port ' + PORT);
-});
-
